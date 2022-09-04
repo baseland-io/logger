bbafa02d4e7a97f08585d679d966eac6f45e172b
Zeph <zephlafassett@xenon.local>
2022-09-03 21:35:51 -0700

    initial commit

---
+++ src/logger.ts
@@ -0,0 +1,44 @@
+import winston, { format } from 'winston';
+
+const UNKNOWN_LABEL = 'UNKNOWN';
+const colorizer = format.colorize();
+const consoleLogFormatter = format.printf((msg) => {
+  let _label = UNKNOWN_LABEL;
+  let _requestId = UNKNOWN_LABEL;
+
+  let { message } = msg;
+  if (msg.requestId) {
+    _requestId = `${msg.requestId.substring(msg.requestId.length - 6)}`;
+  }
+  if (msg.label) {
+    _label = `${msg.label}`;
+  }
+  if (typeof message === 'object') {
+    message = JSON.stringify(message);
+  }
+
+  const line = colorizer.colorize(
+    msg.level,
+    `${msg.timestamp} [${msg.module}] [${msg.level.toUpperCase()}] [${_label}] [${_requestId}]`,
+  );
+  if (msg.stack) {
+    return `${line} - ${msg.stack}`;
+  }
+  return `${line} - ${message}`;
+});
+
+export const log = (filename: string) => winston.createLogger({
+  defaultMeta: {
+    module: filename.replace(process.cwd(), ''),
+  },
+  transports: [
+    new winston.transports.Console({
+      level: 'debug',
+      format: winston.format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.errors({ stack: true }),
+        consoleLogFormatter,
+      ),
+    }),
+  ],
+});
