d5b9ceae09e5cad479bc661b21c75439b8c5ff07
Zeph LaFassett! <zeph@lafassett.com>
2024-01-30 08:49:27 -0800

    [request-logger] these tests don't do anything

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ import type { Config, BaselandLogger } from './lib/logger';
 const logger = (filename: string, config: Config): BaselandLogger => {
   return log(filename, config);
 };
-
+export type { Config };
 export { requestLogger };
 export default logger;
--- tests/index.ts
+++ tests/index.ts
@@ -1,12 +1,69 @@
-import assert from 'assert';
+import log, { requestLogger } from '../src/';
+import express from 'express';
+import request from 'supertest';
 
-function sum(num1, num2) {
-  return num1 + num2;
-}
+import type { Config, BaselandLogger } from '../src/lib/logger';
+import { Express, Request, Response } from 'express-serve-static-core';
 
-describe('Demo test', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    const value = sum(1, 2);
-    assert.equal(value, 3);
+context('tests', async () => {
+  const config: Config = {
+    level: process.env.LOG_LEVEL || 'info',
+  };
+
+  describe('src/lib/logger', () => {
+    let logger: BaselandLogger;
+
+    beforeEach(() => {
+      logger = log('/path/to/file.txt', config);
+    });
+
+    it('should print a simple message to the logger', () => {
+      logger.info('message');
+    });
+
+    it('should print a object message', () => {
+      logger.info({});
+    });
+
+    it('should print an error message and stack', () => {
+      const error = new Error('red alert!');
+      logger.info({ error });
+    });
+
+    it('should print an log message with a `requestId`', () => {
+      const requestId = 'some-request-id';
+      logger.info({ label: 'label', requestId, message: 'has request id' });
+    });
+
+    it('should print an log message with a `method`', () => {
+      logger.info({ label: 'label', method: 'GET', message: 'has http method' });
+    });
+
+    it('should print an log message with a `statusCode`', () => {
+      logger.info({ label: 'label', statusCode: 201, message: 'has http status code' });
+    });
+
+    it('should print an log message with a `duration`', () => {
+      logger.info({ label: 'label', duration: 15, message: 'has request duration' });
+    });
+
+  });
+
+  describe('src/lib/requestLogger', async () => {
+    let logger: BaselandLogger;
+    let app: Express;
+    beforeEach(() => {
+      logger = log('/path/to/file.txt', config);
+    });
+
+    it('should do a thing', async () => {
+      app = express();
+      app.use(requestLogger(logger, { logIpAddress: true, logClientCaller: true }));
+
+      app.get('/', (request: Request, response: Response) => {
+        return response.send('OK');
+      });
+      await request(app).get('/').expect(200);
+    });
   });
 });
